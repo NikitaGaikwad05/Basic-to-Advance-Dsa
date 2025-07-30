e687ebe5010a2a772cb4d92b742ffa64ca2b1123
GaikwadNikita <gaikwadnikitamahadev1@gmail.com>
2025-07-30 23:28:10 +0530

    vowel.java

---
+++ vowel.java
@@ -0,0 +1,16 @@
+import java.util.*;
+ import java.io.*;
+ public class vowel {
+    public static void main(String[] args) {
+        char ch;
+        Scanner sc=new Scanner(System.in);
+        ch=sc.next().charAt(0);
+        if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' || ch=='A' || ch=='E' || ch=='I' ||ch=='O' || ch=='U'){
+            System.out.println("character is vowel"+""+ch);
+        } else{
+            System.out.println("Number is consonent"+ch);
+        }
+
+    }
+    
+}
