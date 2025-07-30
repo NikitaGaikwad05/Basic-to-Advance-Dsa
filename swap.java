85c1ce522acdb084f87ebf8c8d8935616c608e36
GaikwadNikita <gaikwadnikitamahadev1@gmail.com>
2025-07-30 23:09:30 +0530

    swap.java

---
+++ swap.java
@@ -0,0 +1,16 @@
+//using the third variable;
+import java.io.*;
+import java.util.*;
+public class swap {
+    public static void main(String[] args) {
+        int num1,num2,temp;
+        Scanner sc=new Scanner(System.in);
+        num1=sc.nextInt();
+        num2=sc.nextInt();
+        temp=num1;
+        num1=num2;
+        num2=temp;
+        System.out.println(num1+""+num2);
+    }
+    
+}
