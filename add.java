42129987e8e5e4bb84614ba4edfd54fdc017fea5
GaikwadNikita <gaikwadnikitamahadev1@gmail.com>
2025-07-30 22:36:22 +0530

    add.java

---
+++ add.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+public class add
+ {    
+    public static void main(String[] args)
+     {
+        int a,b,c;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Entert the two  number");
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=a+b;
+        System.out.println(c);
+
+    }
+    
+}
