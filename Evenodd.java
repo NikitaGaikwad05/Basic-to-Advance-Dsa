f30ac39e1ebc34cbf0a5f7d17561cde3f74fe9e1
GaikwadNikita <gaikwadnikitamahadev1@gmail.com>
2025-07-30 22:48:18 +0530

    Evenodd.java

---
+++ Evenodd.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+public class Evenodd {
+    public static void main(String[] args) {
+        int num;
+        Scanner sc=new Scanner(System.in);
+         num=sc.nextInt();
+            if(num%2==0){
+                System.out.println("Number is even"+num);
+            }
+            else{
+                System.out.println("NUmber is odd"+num);
+            }
+        
+    }
+    
+}
