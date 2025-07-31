f94f7c3ae0f232afdda15443bf934dc88703b96c
GaikwadNikita <gaikwadnikitamahadev1@gmail.com>
2025-07-31 17:31:20 +0530

    demo.java

--- demo.java
+++ demo.java
@@ -1,5 +1,10 @@
-class demo{
-     public static void main(String[] args) {
-        System.out.println("Hello world");
+public class demo{
+     public void show(){
+           System.out.println("Hello world");
+     
     }
+      public static void main(String[] args) {
+           demo d=new demo();
+           d.show();
+     }
 }
