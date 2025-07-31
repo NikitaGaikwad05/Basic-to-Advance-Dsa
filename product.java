d6673af218f64701d9f37aa51cf91b2fb99eb1df
GaikwadNikita <gaikwadnikitamahadev1@gmail.com>
2025-07-31 17:59:19 +0530

    product.java

--- product.java
+++ product.java
@@ -1,14 +1,17 @@
 import java.util.Scanner;
-import java.io.*;
 
 public class product {
-    public static void main(String[] args) {
+    public void product(){
         int a,b,c;
         Scanner sc=new Scanner(System.in);
         a=sc.nextInt();
         b=sc.nextInt();
         c=a*b;
         System.out.println(c);
+    }
+    public static void main(String[] args) {
+        product p=new product();
+        p.product();
     }
     
 }
