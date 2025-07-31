5ed783914afa7f778fd91ff5b4528f0b9effbadb
GaikwadNikita <gaikwadnikitamahadev1@gmail.com>
2025-07-31 17:35:35 +0530

    add.java

--- add.java
+++ add.java
@@ -1,9 +1,10 @@
 import java.util.*;
 import java.io.*;
 public class add
- {    
-    public static void main(String[] args)
-     {
+
+ { 
+    public void addition()
+    {
         int a,b,c;
         Scanner sc=new Scanner(System.in);
         System.out.println("Entert the two  number");
@@ -12,6 +13,11 @@ public class add
         c=a+b;
         System.out.println(c);
 
+    }    
+    public static void main(String[] args)
+     {
+        add d=new add();
+        d.addition();
     }
     
 }
