9315df4c7ba4f87630a05b2603737526f47479a4
GaikwadNikita <gaikwadnikitamahadev1@gmail.com>
2025-07-31 18:54:13 +0530

    C12.java

---
+++ C12.java
@@ -0,0 +1,15 @@
+//To calculate the table 
+import java.io.*;
+ public class C12{
+    public void mul(){
+        int i=1;
+        while(i<=10){
+            System.out.println(3*i);
+            i=i+1;
+        }
+    }
+    public static void main(String[] args) {
+        C12 c=new C12();
+        c.mul();
+    }
+ }
