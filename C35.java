85e8f9eb1c1a4336fa864a55844dcf69285dee9f
GaikwadNikita <gaikwadnikitamahadev1@gmail.com>
2025-08-06 16:25:38 +0530

    Create C35.java

---
+++ C35.java
@@ -0,0 +1,14 @@
+//looping statement to display series
+import java.io.*;
+import java.util.*;
+public class C35{
+    public void show(){
+        for(int i=1;i<=100;i++){
+            System.out.println(i);
+        }
+    }
+    public static void main(String[]args){
+        C35 c=new C35();
+        c.show();
+    }
+}
