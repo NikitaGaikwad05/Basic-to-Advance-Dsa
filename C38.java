072c4c1d1eb7a4272cfe8c98293385c6af8751c5
GaikwadNikita <gaikwadnikitamahadev1@gmail.com>
2025-08-07 15:05:18 +0530

    Create C38.java

---
+++ C38.java
@@ -0,0 +1,17 @@
+// To display the pattern;
+import java.io.*;
+import java.util.*;
+public class C38{
+    public void show(){
+        for(int i=1;i<=5;i++){
+            for(int j=5;j>=i;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        C38 c=new C38();
+        c.show();
+    }
+}
